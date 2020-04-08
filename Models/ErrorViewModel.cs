e4058d61f0316a069a43b9f8abb7d1065773026d
Vinay Lekhi <vkumar1977@hotmail.com>
2020-04-08 22:30:08 +1000

    INITIAL

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WDSMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
